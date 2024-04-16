46a712efaed61ba341f32e26d28d219a18431ea6
juli868 <bugijul@2001>
2024-04-17 01:48:03 +0200

    Extraction of the credentials.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,4 +6,14 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """Basic authentication class."""
 
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """Extract the encoded credentials."""
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        result = authorization_header.split(' ')
+        return result[1]
