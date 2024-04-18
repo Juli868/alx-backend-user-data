e9067ef4c189dddedbb170723d75a73a74bdcbad
juli868 <bugijul@2001>
2024-04-19 01:57:02 +0200

    Decoding

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Authentication base."""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -17,3 +18,17 @@ class BasicAuth(Auth):
             return None
         result = authorization_header.split(' ')
         return result[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Decode a base64 header to find the credentials."""
+        if base_64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            primary = base64.b64decode(base64_authorization_header)
+            res = primary.decode('utf-8')
+            return res
+        except(TypeError, UnicodeDecodeError):
+            return None
