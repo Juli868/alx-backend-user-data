418f3e88abfb982c37ab22927cc480b83536641e
juli868 <bugijul@2001>
2024-04-18 20:47:25 +0200

    Session creation.

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
 """Authorize accordingly."""
 from api.v1.auth.basic_auth import Auth
+import uuid
+
+
 class SessionAuth(Auth):
     """Authorizing according to the session."""
-    pass
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create a session according to the user id."""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = uuid.uuid4()
+        user_id_by_session_id = {seesion_id: userid}
+        return session_id
