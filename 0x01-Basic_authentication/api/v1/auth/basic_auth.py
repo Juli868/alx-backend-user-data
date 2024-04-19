caa260be8715c1b7d926736d1d33169f13b06a3f
juli868 <bugijul@2001>
2024-04-19 02:32:41 +0200

    Credentials stripping.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """Authentication base."""
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -32,3 +33,27 @@ class BasicAuth(Auth):
             return res
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """Determine the credential separately, i.e: username and password."""
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        result = decoded_base64_authorization_header.split(':')
+        return(str(result[0]), str(result[1]))
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """Provode the credibility for the user, if exists in the db."""
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Retrieve the user instance for a request."""
+        pass
