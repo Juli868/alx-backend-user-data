f84e07969328edbc07766a12bd1fc957dfc7dfc4
juli868 <bugijul@2001>
2024-04-25 23:06:48 +0200

    Home page addition.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+"""Run my application on a web."""
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    """Page for home."""
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -10,12 +10,14 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode(), salt)
 
+
 class Auth:
-    """Auth class to interact with the authentication database.
-    """
+    """Auth class to interact with the authentication database."""
 
     def __init__(self):
+        """Initialise a class."""
         self._db = DB()
+
     def register_user(self, email: str, password: str) -> User:
         """Create a user with given credentials."""
         existing_user = self._db.find_user_by(email=email)
